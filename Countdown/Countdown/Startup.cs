8b37bac065d4bd9a89ec50dd789f2bd998a8667e
Tomas Hill <35506995+hilltw@users.noreply.github.com>
2019-11-03 13:57:33 -0500

    Initial project setup with updated dependencies.  ASP .NET MVC (.Net Framework).

---
+++ Countdown/Countdown/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Countdown.Startup))]
+namespace Countdown
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
